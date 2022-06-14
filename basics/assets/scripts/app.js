ea2c1458526d7830cdf4fb53fc863dd262738408
unknown <markopetko94@gmail.com>
2022-06-14 09:09:09 +0200

    short theory for variables, constants & operaters

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,1 +1,39 @@
-alert('Hello World'); // alert message
+// Variables, Constants & Operaters
+
+/* Variables - data container/storage where the value can change
+   
+   keyword - let
+
+   Variable Naming - camelCase
+*/
+
+// Example
+
+let currentResult = 0;
+
+/* Constants - data container/storage where the value must not change
+
+   keyword - const
+
+   Constant Naming - camelCase
+*/
+
+// Example
+
+const defaultResult = 0;
+
+/* Operators
+
++ Add two numbers
+
+- Substract two numbers
+
+- Multiply two numbers
+
+/ Devide two numbers
+
+% Devide two numbers, yield remainder
+
+** Exponentiation (e.g. 2 ** 3 = 8)
+
+*/
