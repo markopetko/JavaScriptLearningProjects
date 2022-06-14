9f618a1255e3deb3a01a89ae48005371390a2f99
unknown <markopetko94@gmail.com>
2022-06-14 09:19:10 +0200

    add two numbers example

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,39 +1,8 @@
-// Variables, Constants & Operaters
-
-/* Variables - data container/storage where the value can change
-   
-   keyword - let
-
-   Variable Naming - camelCase
-*/
-
-// Example
-
 let currentResult = 0;
 
-/* Constants - data container/storage where the value must not change
+currentResult = currentResult + 10; // Add two numbers
 
-   keyword - const
+outputResult(currentResult, '');
 
-   Constant Naming - camelCase
-*/
+// Result = 10
 
-// Example
-
-const defaultResult = 0;
-
-/* Operators
-
-+ Add two numbers
-
-- Substract two numbers
-
-- Multiply two numbers
-
-/ Devide two numbers
-
-% Devide two numbers, yield remainder
-
-** Exponentiation (e.g. 2 ** 3 = 8)
-
-*/
