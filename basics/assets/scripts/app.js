2c541a4612cc67d8bf25dc6236d533b0e7400f3f
unknown <markopetko94@gmail.com>
2022-06-20 09:14:25 +0200

    basic example for using numbers and strings

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,8 +1,18 @@
+/* 
+    Numbers - 2, -3, 3.5426
+   
+    Strings - text characters
+*/
+
 let currentResult = 0;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1; // other mathematical examples
+currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-outputResult(currentResult, '');
+let calculationDescription = '(' + currentResult + ' + 10) * 3 / 2 - 1';
 
-// Result = 14
+outputResult(currentResult, calculationDescription);
 
+/* Output result:
+    (14 + 10) * 3 / 2 - 1
+    Result: 14
+*/
