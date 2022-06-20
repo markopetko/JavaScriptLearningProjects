ce49a672aeb5a85fbdd5f8486d277a127d8283c7
unknown <markopetko94@gmail.com>
2022-06-20 09:20:13 +0200

    more about using strings - 1

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,5 +1,5 @@
 /* 
-    More about using constants
+    More about using strings - 1
 */
 
 const defaultResult = 0;
@@ -8,13 +8,13 @@ let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1'; // using constant inside string
+let calculationDescription = "'('" + defaultResult + ' + 10) * 3 / 2 - 1'; // using " "
 
 outputResult(currentResult, calculationDescription);
 
 /* Output result:
 
-    (0 + 10) * 3 / 2 - 1
+    '('0 + 10) * 3 / 2 - 1
 
     Result: 14
 */
