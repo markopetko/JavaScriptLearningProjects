c6ca7ae59062130a2f0696eedee7ba09a1cba522
unknown <markopetko94@gmail.com>
2022-06-24 08:38:40 +0200

    assignment - 1

---
+++ assignments/assignment 1/assignment.js
@@ -0,0 +1,27 @@
+// #1
+
+let userInput = 30;
+let result;
+
+// #2
+
+result = 18 + userInput;
+
+// #3
+
+result = result - 10;
+
+result = result * 2;
+
+result = result / 4;
+
+// #4
+
+/* userInput did not change thru the code, but it can be changed because that variable is not constant, 
+   to set that variable to constant in that case will be const userInput = 30; */
+
+// #5
+
+alert(userInput); // alert = 30
+
+alert(result); // alert = 19
