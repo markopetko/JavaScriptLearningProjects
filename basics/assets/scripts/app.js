ecb564b65298ca332aa19f775e7f32f4ee6b930f
unknown <markopetko94@gmail.com>
2022-07-07 09:18:06 +0200

    parseInt / parseFloat / + +   - basic calculator 2

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,16 +1,22 @@
 /* 
-    Executing functions indirectly - basic calculator - 1
+    Executing functions indirectly - basic calculator - 2
 */
+
 const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-    currentResult = currentResult + userInput.value;
+    currentResult = currentResult + parseInt(userInput.value); 
+    /* all other SIMPLE operators will work, just + operator wound't, and because of that must be used parseInt / parseFloat / ++ 
+       parseInt converting string number to an number without decimal places
+       parseFloat converting string number to an number with decimal places */
+
+    // currentResult = currentResult + + userInput.value; // another way converting string number to an number with + +
     outputResult(currentResult, '');
 }
 
 addBtn.addEventListener('click', add); // ('event', which event is called)
 
 /* Output result:
-    If you input 5, result is: 05
+    If you input 10, result is: 10
 */
